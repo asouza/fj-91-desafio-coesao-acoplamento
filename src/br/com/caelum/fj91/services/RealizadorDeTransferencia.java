6701354d560ee056271a0acfc4292d480fe0e0d6
asouza <alots.ssa@gmail.com>
2016-03-28 18:17:52 -0300

    minors

--- src/br/com/caelum/fj91/services/RealizadorDeTransferencia.java
+++ src/br/com/caelum/fj91/services/RealizadorDeTransferencia.java
@@ -12,22 +12,20 @@ public class RealizadorDeTransferencia {
 	public void executa(Conta conta1, Conta conta2, BigDecimal valor) {
 		conta1.transfere(conta2, valor);
 		try {
-			new Mailer(false).send("banco@gmail.com", conta1.getCliente().getEmail(),
-					"Nova transferencia", "corpo do email");
+			new Mailer(false).send("banco@gmail.com", conta1.getCliente()
+					.getEmail(), "Nova transferencia", "corpo do email");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		TransferenciaDao transferenciaDao;
+
 		try {
-			transferenciaDao = new TransferenciaDao(false);
-			transferenciaDao.salva(conta1,conta2,valor);
+			TransferenciaDao transferenciaDao = new TransferenciaDao(false);
+			transferenciaDao.salva(conta1, conta2, valor);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
 	}
 
 }
